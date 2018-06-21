bff968ccfc9971b293b87aa07f8ddc09d3f2fead
Zylanx <ZylanxDragon@gmail.com>
2018-06-22 03:45:48 +1000

    Update Code and Add Some Comments There is still plenty of comments left to be added Should however give a little bit more info than what was there previously

--- eval_ast_gen.py
+++ eval_ast_gen.py
@@ -10,6 +10,14 @@ __author__ = "Zylanx"
 
 
 class OutputExprRewriter(ast.NodeTransformer):
+    """
+        OutputExprRewriter: This transformer runs through every top level statement and wraps them in
+                            so they send their result to the function "outputExpr".
+                            This is the basis for the "Interactive Interpreter" style of return value display
+                            It also removes "await" statements, just leaving the
+                            expression afterwards (which it proceeds to process)
+    """
+    
     def visit_FunctionDef(self, node):
         self.generic_visit(node)
         return node
@@ -27,21 +35,26 @@ class OutputExprRewriter(ast.NodeTransformer):
             args = node.value
         
         call = ast.Call(ast.Name("outputExpr", ast.Load()), args, [])
-        newnode = ast.Expr(value=call)
-        ast.copy_location(newnode, node)
-        ast.fix_missing_locations(newnode)
-        self.generic_visit(newnode)
-        return newnode
+        newNode = ast.Expr(value=call)
+        ast.copy_location(newNode, node)
+        ast.fix_missing_locations(newNode)
+        self.generic_visit(newNode)
+        return newNode
     
-    def vist_Await(self, node):
-        newnode = node.value
-        ast.copy_location(newnode, node)
-        ast.fix_missing_locations(newnode)
-        self.generic_visit(newnode)
-        return newnode
+    def visit_Await(self, node):
+        newNode = node.value
+        ast.copy_location(newNode, node)
+        ast.fix_missing_locations(newNode)
+        self.generic_visit(newNode)
+        return newNode
 
 
 class FunctionAsyncRewriter(ast.NodeTransformer):
+    """
+        FunctionAsyncRewriter: This transformer runs through the AST and redirects all function calls
+                               to be wrapped by the "callFuncExec" function
+    """
+    
     def visit_Call(self, node):
         if not isinstance(node.args, list):
             if not node.args:
@@ -60,6 +73,13 @@ class FunctionAsyncRewriter(ast.NodeTransformer):
 
 
 class FinishedSigWrapper(ast.NodeTransformer):
+    """
+        FinishedSigWrapper: This transformer wraps the modified code in some extra code to deal with communicating
+                            the execution to the outside world and signaling to the future that it is now done
+                            and the command has completed, or if there is a failure, that an exception has
+                            occurred
+    """
+    
     def visit_Module(self, node):
         setDoneNode = ast.Expr(ast.Call(ast.Attribute(ast.Name("finishedExecSig", ast.Load()), "set_result", ast.Load()), [ast.NameConstant(None)], []))
         setExceptionNode = ast.Expr(ast.Call(ast.Attribute(ast.Name("finishedExecSig", ast.Load()), "set_exception", ast.Load()), [ast.Name("e", ast.Load())], []))
@@ -70,17 +90,24 @@ class FinishedSigWrapper(ast.NodeTransformer):
         
         tryNode = ast.Try(mainBody, [tryExceptNode], [], [])
         
-        newnode = ast.Module([tryNode])
-        ast.copy_location(newnode, node)
-        ast.fix_missing_locations(newnode)
-        return newnode
+        newNode = ast.Module([tryNode])
+        ast.copy_location(newNode, node)
+        ast.fix_missing_locations(newNode)
+        return newNode
 
 
+# TODO: Comment outputExpr
 def outputExpr(value):
+    """
+        outputExpr: Top level expressions are wrapped by a call to this function.
+                    This function simply prints the repr of the result of the wrapped expression.
+    """
     if value is not None:
         print(repr(value))
 
 
+# TODO: Comment OutputExpr
+# COMMENT: OutputExpr is just left over from before the stdout was smart piped like it is now
 class OutputExpr:
     def __init__(self, pipe):
         self.pipe = pipe
@@ -90,24 +117,38 @@ class OutputExpr:
             print(repr(value), file=self.pipe)
 
 
+#  WARNING: This function messes with the internal asyncio 
+#	        event loop in ways it shouldn't. Use at your own discretion! 
 def callFuncExec(func, *args, **kwargs):
-    """ WARNING: This function messes with the internal event loop in
-                 ways it shouldn't. Use at your own discretion! """
+    """
+        callFuncExec: This function does most of the heavy lifting for the library
+                      It takes in a function and depending on whether it is a normal function
+                      or a coroutine, either execute it normally, or otherwise take
+                      control of the asyncio event loop and run it synchronously.
+    """
+    
+    # If nothing passed in, then there is a fatal error and it needs to exit
     if not func:
         raise Exception("No function passed in")
     
-    loop = asyncio.get_event_loop()
+    # If the function is a coroutine, add the function to the event
+    # loop then step through the loop until the future has completed
     if asyncio.iscoroutinefunction(func):
-        fut = asyncio.ensure_future(func(*args, **kwargs))
+        loop = asyncio.get_event_loop()
+        fut = asyncio.ensure_future(func(*args, **kwargs)) # Adds the func as a future to the loop 
+        
+        # Leaving our managed code so redirect stdout back to system
         with redirect_stdout(sys.__stdout__):
-            while not fut.done():
+            while not fut.done(): # loop until the future is ready
                 loop._run_once()
         result = fut.result()
-    else:
+    else: # Normal function. Just execute as normal
         result = func(*args, **kwargs)
     return result
 
 
+# TODO: Comment fixASTAwaitError
+# TODO: Strip awaits from even more places
 def fixASTAwaitError(text, offset):
     tokenList = list(tokenize.tokenize(io.BytesIO(text.encode("utf-8")).readline))
     
@@ -170,6 +211,7 @@ def fixASTAwaitError(text, offset):
     return None
 
 
+# TODO: Comment parseAST
 def parseAST(inputText):
     for _ in range(50):
         try:
--- exec_command.py
+++ exec_command.py
@@ -1,9 +1,16 @@
 # This was made for discord.py "async".
 # It should still work on discord.py "rewrite", but will just need some small edits
 # It has not been tested on discord.py "rewrite"
+
+import io
+import re
+from functools import partial
+from contextlib import redirect_stdout
+import traceback
+
 import discord
 if discord.version_info.releaselevel == "alpha":
-    print('This was made for discord.py "async".\nIt should still work on discord.py "rewrite", but will just need some small edits\nIt has not been tested on discord.py "rewrite"\')
+    print('This was made for discord.py "async".\nIt should still work on discord.py "rewrite", but will just need some small edits\nIt has not been tested on discord.py "rewrite"\n')
 
 import asyncio
 import eval_ast_gen
@@ -11,92 +18,175 @@ import eval_ast_gen
 __author__ = "Zylanx"
 
 
+# Config options for the Example Bot
+botToken = ""
+ownerID = ""
+commandPrefix = "!"
+
+
+# This code is mainly meant as a demonstration. There are
+#   however some parts that are _needed_. This will be
+#   tagged as such ("-REQUIRED-") (This has not been done yet TODO: Mark sections as "-REQUIRED-")
+
+# ------ The Command --------
+
+
+# TODO: Finish commenting commandEval
 async def commandEval(self, message: discord.Message, commandText: str):        
-    commandText = commandText.strip("\n")
-    
-    def appendReply(env, *args, **kwargs):
-        embed = None
-        if "embed" in kwargs:
-            embed = kwargs["embed"]
-        if not args and not embed:
-            raise TypeError("appendReply: No message or embed passed in")
-        if not args:
-            args = [""]
-        eval_ast_gen.callFuncExec(self.edit_message, env["execStartMessage"], str(env["execStartMessage"].content) + "\n" + str(args[0]), embed=embed)
-    
-    def execAST(inputAST, env):
-        # Send a message to the channel for the users code to edit if wanted
-        env["execStartMessage"] = eval_ast_gen.callFuncExec(self.send_message, message.channel, "`--Eval Started--`\n")
-        env["appendReply"] = partial(appendReply, env)
-        
-        # Actually execute the AST
-        try:
-            exec(compile(inputAST, "<ast>", "exec"), env)
-        except Exception as e:
-            env["finishedExecSig"].set_exception(e)
-    
-    sigDone = asyncio.Future()
-    
-    with io.StringIO() as outputExprPipe, io.StringIO() as evalStdout:
-        outputExprInst = eval_ast_gen.OutputExpr(outputExprPipe)
-        
-        env = {
-            'bot' : self,
-            'client' : self,
-            'msg' : message,
-            'message' : message,
-            'content' : message.content,
-            'guild' : message.server,
-            'channel' : message.channel,
-            'me' : message.author,
+	"""  """
+	
+	commandText = commandText.strip("\n")
+	
+	def appendReply(env, *args, **kwargs):
+		"""  """
+		embed = None
+		if "embed" in kwargs:
+			embed = kwargs["embed"]
+		if not args and not embed:
+			raise TypeError("appendReply: No message or embed passed in")
+		if not args:
+			args = [""]
+		eval_ast_gen.callFuncExec(self.edit_message, env["execStartMessage"], str(env["execStartMessage"].content) + "\n" + str(args[0]), embed=embed)
+	
+	def execAST(inputAST, env):
+		"""  """
+		
+		# Send a message (synchronously) to the channel that user code has access to
+		env["execStartMessage"] = eval_ast_gen.callFuncExec(self.send_message, message.channel, "`--Eval Started--`\n")
+		env["appendReply"] = partial(appendReply, env)
+		
+		# Execute the AST
+		try:
+			exec(compile(inputAST, "<ast>", "exec"), env)
+		except Exception as e:
+			env["finishedExecSig"].set_exception(e)
+	
+	# Signal
+	sigDone = asyncio.Future()
+	
+	# Setup the pipes for redirecting output
+	with io.StringIO() as outputExprPipe, io.StringIO() as evalStdout:
+		outputExprInst = eval_ast_gen.OutputExpr(outputExprPipe)
+		
+		env = {
+			# Base Globals
+			'bot' : self,
+			'client' : self,
+			'msg' : message,
+			'message' : message,
+			'content' : message.content,
+			'guild' : message.server,
+			'channel' : message.channel,
+			'me' : message.author,
+			'author' : message.author,
+			'caller' : message.author,
 
-            # utilities
-            '_get' : discord.utils.get,
-            '_find' : discord.utils.find,
-            'reply' : partial(self.addToReplyQueue, message),
-            'sendHere' : partial(eval_ast_gen.callFuncExec, asyncio.coroutine(partial(self.send_message, message.channel))),
-            'execStartMessage' : None,
-            'appendReply' : None,
-            
-            # Previous Executions
-            'lastResult' : self.evalLastResult,
-            'lastCodeText' : self.evalLastCode,
-            'lastAST' : self.evalLastAST,
-            
-            # Exec Utils
-            'outputExpr' : outputExprInst.printExpr,
-            'callFuncExec' : eval_ast_gen.callFuncExec,
-            'finishedExecSig' : sigDone,
-            'outputExprPipe' : outputExprInst.pipe,
-        }
-        env.update(globals())
-        
-        try:
-            outAST = eval_ast_gen.parseAST(commandText)
-            with redirect_stdout(evalStdout):
-                loop = asyncio.get_event_loop()
-                handle = asyncio.events.Handle(execAST, (outAST, env), loop)
-                loop._ready.insert(0, handle)
-                await sigDone
-        except Exception as e:
-            await self.send_message(message.channel, "```py\n{}\n```".format(traceback.format_exc(limit=1)))
-            return
+			# Utilities
+			'_get' : discord.utils.get,
+			'_find' : discord.utils.find,
+			'reply' : partial(eval_ast_gen.callFuncExec, asyncio.coroutine(partial(self.send_message, message.channel))),
+			'sendHere' : partial(eval_ast_gen.callFuncExec, asyncio.coroutine(partial(self.send_message, message.channel))),
+			'waitForMessage' : self.wait_for_message,
+			'execStartMessage' : None,
+			'appendReply' : None,
+			
+			# Previous Executions
+			'lastResult' : self.evalLastResult,
+			'lastCodeText' : self.evalLastCode,
+			'lastAST' : self.evalLastAST,
+			
+			# Exec Utils
+			'outputExpr' : outputExprInst.printExpr,
+			'callFuncExec' : eval_ast_gen.callFuncExec,
+			'finishedExecSig' : sigDone,
+			'outputExprPipe' : outputExprInst.pipe,
+		}
+		env.update(globals())
+		
+		try:
+			outAST = eval_ast_gen.parseAST(commandText)
+			with redirect_stdout(evalStdout):
+				loop = asyncio.get_event_loop()
+				handle = asyncio.events.Handle(execAST, (outAST, env), loop)
+				loop._ready.insert(0, handle)
+				await sigDone
+		except Exception as e:
+			# This allows the executing code's error to be printed
+			# TODO: Research if the traceback can have its context set to the executed code
+			await self.send_message(message.channel, "```py\n{}\n```".format(traceback.format_exc(limit=0)))
+			return
 
-        stdoutResult = evalStdout.getvalue()
-        outputExprResult = outputExprInst.pipe.getvalue()
+		stdoutResult = evalStdout.getvalue()
+		outputExprResult = outputExprInst.pipe.getvalue()
 
-    await self.send_message(message.channel, "Eval Successful")
+	await self.send_message(message.channel, "Eval Successful")
 
-    if stdoutResult is not None:
-        self.evalLastResult = stdoutResult
-        
-    if outAST is not None:
-        self.evalLastAST = outAST
-        
-    self.evalLastCodeText = commandText
+	if stdoutResult is not None:
+		self.evalLastResult = stdoutResult
+		
+	if outAST is not None:
+		self.evalLastAST = outAST
+		
+	self.evalLastCodeText = commandText
 
-    formattedOut = "```py\n---Stdout---\n{}\n---Expr Vals---\n{}\n```".format(stdoutResult, outputExprResult)
-    if len(formattedOut) > 2000:
-        await self.send_message(message.channel, "Too long to post results")
-    else:
-        await self.send_message(message.channel, formattedOut)
+	formattedOut = "```py\n---Stdout---\n{}\n---Expr Vals---\n{}\n```".format(stdoutResult, outputExprResult)
+	if len(formattedOut) > 2000:
+		await self.send_message(message.channel, "Too long to post results")
+	else:
+		await self.send_message(message.channel, formattedOut)
+
+
+# ------ A Bot To Show It Off --------
+
+
+# TODO: Comment ExecExampleBot
+class ExecExampleBot(discord.Client):
+
+	def __init__(self, botOwnerID=None, commandPrefix=None, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		
+		if botOwnerID is None:
+			botOwnerID = ""
+		if commandPrefix is None:
+			commandPrefix = "!"
+		
+		self.botOwnerID = botOwnerID
+		self.commandPrefix = commandPrefix
+
+		self.evalRegex = re.compile(r"```(?:py ?)?(.+)```", re.DOTALL)
+		self.evalLastResult = None
+		self.evalLastCode = None
+		self.evalLastAST = None
+
+	async def on_ready(self):
+		print("!!!Bot connected and ready!!!\n")
+		self.botOwner = await self.get_user_info(self.botOwnerID)
+
+	async def on_message(self, message: discord.Message):
+		if message.author.bot or (message.author == self.user): # Don't process own or other bots messages
+			return
+
+		if message.author.id == self.botOwnerID:
+			if message.content.startswith(self.commandPrefix + "shutdown"):
+				print("\n!!!Bot shutting down!!!")
+				await self.send_message(message.channel, "Bot shutting down")
+				await self.logout()
+				return
+			elif message.content.startswith(self.commandPrefix + "exec") or message.content.startswith(self.commandPrefix + "eval"):
+				match = self.evalRegex.search(message.content)
+				if match is None:
+					await self.send_message(message.channel, "Eval was malformed. Please wrap code in triple-quoted code blocks.")
+					return
+				else:
+					await self.commandEval(message, match.group(1))
+					return
+	
+	commandEval = commandEval
+
+
+if __name__ == "__main__":
+	if not botToken or not ownerID:
+		raise Exception("Please provide a Bot Token and your Account ID")
+	print("Starting bot")
+	execBot = ExecExampleBot(botOwnerID=ownerID, commandPrefix=commandPrefix)
+	execBot.run(botToken)
